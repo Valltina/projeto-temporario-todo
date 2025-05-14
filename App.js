f9504603d1c990cc66e69f079013249ab7bacfc6
Valentina de Oliveira Mendes <valentinaoliveira918@gmail.com>
2025-05-14 11:53:39 +0000

    [feat]Add Input Text

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, FlatList } from 'react-native';
 import logo from "./assets/LOGOTODO2.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
-        <Image source={logo} style={styles.logo}/>
-        <Text>TODO list</Text>
+      <View style={styles.viewLogo}>
+        <Image source={logo} style={styles.logo} />
+        <Text style={styles.textLogo}>TODO list</Text>
       </View>
+      <View style={styles.viewTask}>
+        <TextInput style={styles.inputTask} placeholder="Digite sua tarefa"/>
+        <Button title="Adicionar"/>
+        </View>
+        <FlatList style={styles.taskList}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,8 +25,36 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
   logo: {
-    height: 100,
+    height: 150,
+    width: 150,
   },
+
+  viewTask: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+
+  inputTask: {
+    flex: 1,
+    borderColor: "gray",
+    borderWidth: 1,
+    paddingLeft: 10,
+    marginRight: 10,
+  },
+
+  viewLogo: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  textLogo: {
+    justifyContent: "center",
+  },
+
+  taskList: {
+    flex: 1,
+  }
 });
